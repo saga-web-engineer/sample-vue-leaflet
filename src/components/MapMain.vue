5cf8aabdfce1c2cf4d7634581de0708eb5e27041
kumeyuta <ku8.m.1.e2@gmail.com>
2025-02-18 01:25:30 +0900

    スケールバー追加

--- src/components/MapMain.vue
+++ src/components/MapMain.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet';
+import { LControlScale, LMap, LTileLayer } from '@vue-leaflet/vue-leaflet';
 import type { PointExpression } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { ref } from 'vue';
 
-const center = ref<PointExpression>([33.2641808, 130.2948219]);
+// 佐賀駅を初期中心座標
+const initCenter = ref<PointExpression>([33.2641808, 130.2948219]);
+const center = initCenter.value;
 const zoom = ref(12);
 const mapUrl = 'https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png';
 const mapOptions = {
@@ -15,6 +17,7 @@ const mapOptions = {
 <template>
   <l-map :zoom="zoom" :center="center" :use-global-leaflet="false" :options="mapOptions">
     <l-tile-layer :url="mapUrl"></l-tile-layer>
+    <l-control-scale position="bottomright" :imperial="false" :metric="true"></l-control-scale>
   </l-map>
 </template>
 
